8cad0e250ed12963656eab8818c5bb8e073ebf45
havsgard <60538559+havsgard@users.noreply.github.com>
2020-04-20 14:06:41 +0200

    Add files via upload

---
+++ anders-havsgaard-js1-ma3.js
@@ -0,0 +1,44 @@
+// question 1
+
+const sum = (a, b) => a + b;
+
+
+// question 2
+
+const Url = `https://api.rawg.io/api/games?genres=sports`;
+
+fetch(Url)
+  .then(data => {
+    return data.json();
+  })
+  .then(Response => {
+    console.log(Response);
+  });
+
+
+// question 3
+
+var str = "These cats are outrageous.";
+var res = str.replace("cats", "giraffes");
+
+
+// question 4
+
+
+
+// question 5
+
+<div class="container">
+    <p>Lorem ipsum dolor</p>
+    <button class="btn">Click me</button>
+</div>
+
+container.removeChild(button);
+
+
+// question 6
+
+
+
+// question 7
+
